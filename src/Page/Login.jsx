569b41aab58132bb0e8bf349cc60cc60c1ead718
junandar1912 <jujunjunandar79@gmail.com>
2025-08-20 06:33:43 +0700

    fix login

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,34 +1,101 @@
-import React from 'react'
-import Header from '../Components/Header'
-import LoginForm from '../form/LoginForm.jsx';
+import React from 'react';
+import Header from '../Components/Header';
+import Button from '../Components/Button.jsx';
+import Input from '../Components/Input.jsx';
+import Inputpassword from '../Components/Inputpassword.jsx';
 
-const LoginFormData = {
-  judul: "Login"
-}
+const InputData = [
+  {
+    label: "Username",
+    placeholder: "Enter your username",
+    type: "text",
+  },
+  {
+    label: "Email",
+    placeholder: "Enter your email",
+    type: "email",
+  },
+];
+
+const InputPasswordData = [
+  {
+    label: "Password",
+    placeholder: "Enter your password"
+  }
+];
 
 const Login = () => {
   return (
     <>
-    <div className='min-h-screen bg-gradient-to-r from-[#77CEf38c] via-[#CDF5FD] to-[#00A9FF]'>
-     <Header />
-       <main className='bg-transparent p-10 rounded-tl-[50px] 
-       flex flex-row justify-center'>
-        <div className='flex flex-col gap-8 w-full justify-center items-center'>
-          <img src="../src/assets/cmlabs-login-icon.svg" alt="logo-login" 
-          className='h-[542px] w-[532px]'/>
-          <p className='font-raleway italic font-bold text-4xl text-chart-3 text-right'>
-            Masuk Dengan Aman <br /> Kelola Dengan Percaya Diri</p>
-        </div>
-        <img src="../src/assets/cmlabs-logo.svg" alt="logo-app" 
-          className="h-[272px] w-[272px] self-center" />
-        <div className='flex flex-row justify-center h-auto 
-        pl-14 pr-14 w-full items-center'>
-          <LoginForm {...LoginFormData} />
-        </div>
-       </main>
-    </div>
+      <div className='min-h-screen bg-gradient-to-r from-[#77CEf38c] via-[#CDF5FD] to-[#00A9FF]'>
+        <Header />
+        <main className='bg-transparent p-10 rounded-tl-[50px] flex flex-row justify-center'>
+          <div className='flex flex-col gap-8 w-full justify-center items-center'>
+            <img
+              src="../src/assets/cmlabs-login-icon.svg"
+              alt="logo-login"
+              className='h-[542px] w-[532px]'
+            />
+            <p className='font-raleway italic font-bold text-4xl text-chart-3 text-right'>
+              Masuk Dengan Aman <br /> Kelola Dengan Percaya Diri
+            </p>
+          </div>
+          <img
+            src="../src/assets/cmlabs-logo.svg"
+            alt="logo-app"
+            className="h-[272px] w-[272px] self-center"
+          />
+          <div className='flex flex-row justify-center h-auto pl-14 pr-14 w-full items-center'>
+            <form className='flex flex-col gap-2.5 h-full w-full shadow-inner rounded-4xl p-7 bg-[#CDF5FD]'>
+              <header className='flex flex-col self-start gap-10'>
+                <h1 className='font-inter font-bold text-5xl'>Login</h1>
+                <p className='font-lato text-2xl text-[Black-900 (#1A1A1A)]'>Selamat Datang Kembali</p>
+              </header>
+              <div>
+                {InputData.map((input, index) => (
+                  <Input
+                    key={index}
+                    label={input.label}
+                    placeholder={input.placeholder}
+                    type={input.type}
+                    value={input.value}
+                    onChange={input.onChange}
+                  />
+                ))}
+                {InputPasswordData.map((input, index) => (
+                  <Inputpassword
+                    key={index}
+                    label={input.label}
+                    placeholder={input.placeholder}
+                  />
+                ))}
+              </div>
+              <div className='self-end m-2.5'>
+                <a to="/forgot-password" className='font-lato text-base text-blue-500 cursor-pointer'>
+                  Lupa Password?
+                </a>
+              </div>
+              <div className='flex flex-col gap-5'>
+                <Button
+                  text="Login"
+                  color="bg-blue-500"
+                />
+                <Button
+                  text="Daftar Login Dengan Akun Google"
+                  color="bg-[#333333]"
+                  image="../src/assets/Google.svg"
+                />
+              </div>
+              <p className='font-lato text-base text-[Black-900 (#1A1A1A)] self-center mt-2.5'>Belum Punya Akun?   
+                <a href="" className='text-blue-500 ml-1.5 cursor-pointer'>Daftar Disini</a>
+              </p>
+            </form>
+          </div>
+        </main>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+
+export default Login;
