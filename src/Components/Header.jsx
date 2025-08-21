97ee8c182144754b576305a9e88d241be90f3372
junandar1912 <jujunjunandar79@gmail.com>
2025-08-21 11:06:21 +0700

    fix header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,8 @@ const Header = () => {
   return (
     <div className='lg:h-[190px] h-20 w-auto bg-[#77CEF38C] rounded-bl-xl-[50px] rounded-br-[50px] p-5'>
       <img src="../src/assets/cmlabs-logo-new.svg" alt="logo" 
-      className='lg:h-132px lg:w-517px h-12 w-48 
+      className='lg:h-[132px] lg:w-[517px] 
+      h-12 w-48 
       bg-transparent rounded-4xl'/>
     </div>
   )
