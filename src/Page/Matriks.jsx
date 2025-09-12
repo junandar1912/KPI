a71902dda1440769e8a3fcd281e2e93327d5e94c
junandar1912 <jujunjunandar79@gmail.com>
2025-09-12 13:47:54 +0700

    matriks

--- src/Page/Matriks.jsx
+++ src/Page/Matriks.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import HeaderMenu from '../Components/Menu/HeaderMenu'
 import Sidemenu from '../Components/Menu/Sidemenu'
 import NavMenu from '../Components/Menu/NavMenu'
@@ -6,12 +6,31 @@ import InputPenambahan from '../Components/Menu/InputPenambahan'
 import DaftarMatriks from '../Components/KolomTable-data/DaftarMatriks'
 
 const Matriks = () => {
+
+  const activeJudul = 'Matriks KPI'
+  const [MarginSidebar, setMarginSidebar] = useState(false);
+  const [isiSidebarTerlihat, setSidebarTidakterlihat] = useState(false);
+
+  const TombolSidebar = useCallback(() => {
+    setSidebarTidakterlihat((s) => !s);
+    setMarginSidebar((s) => !s);
+  }, [])
+
+
   return (
     <div className='flex flex-col gap-8 h-min-screen w-full bg-[#FAFAFA]'>
-      <HeaderMenu/>
-      <main className={`flex flex-col gap-8 p-11 h-min-screen`}>
+      <HeaderMenu
+      activeJudul={activeJudul}
+      TombolSidebar={ TombolSidebar }
+      isiSidebarTerlihat={ isiSidebarTerlihat }
+      MarginSidebar={ MarginSidebar }/>
+      <main className={`flex flex-col gap-8 p-11 h-min-screen ${
+        MarginSidebar ? 'ml-56' : 'ml-0'}`}>
+        <Sidemenu/>
         <InputPenambahan/>
         <DaftarMatriks/>
+        <NavMenu/>
+        <InputPenambahan/>
       </main>
     </div>
   )
